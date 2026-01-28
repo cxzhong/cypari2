5273a890d303b0c92f3df3e8db486481f1bac85f
Chenxin Zhong <chenxin.zhong@outlook.com>
2026-01-28 20:31:49 +0800

    Ensure matrix is on heap and fix avma assignment
    Fix memory management by ensuring matrix is on heap before assignment and correct avma assignment.

--- cypari2/pari_instance.pyx
+++ cypari2/pari_instance.pyx
@@ -1265,6 +1265,8 @@ cdef class Pari(Pari_auto):
             if len(entries) != m * n:
                 raise IndexError("len of entries (=%s) must be %s*%s=%s" % (len(entries), m, n, m*n))
             k = 0
+            # Ensure the matrix is on the heap before assigning cloned entries
+            A.fixGEN()
             for i in range(m):
                 for j in range(n):
                     sig_check()
